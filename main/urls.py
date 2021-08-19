9b9eb0cf00cb338bcc14ad39cdb248d63f865698
Eragorn <filipflis@wp.pl>
2021-08-19 15:34:23 +0200

    add debuging time now endpoint

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.basic_view),
     path('prezent_dla_rudego', views.prezent_view),
+    path('time_now', views.time_now),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,3 +10,7 @@ def basic_view(request):
 
 def prezent_view(request):
     return render(request, 'prezent.html')
+
+def time_now(request):
+    return HttpResponse(str(datetime.datetime.now()))
+    #return render(request, 'prezent2.html')
